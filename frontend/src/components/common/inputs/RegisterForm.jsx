b3229ee3efb051c814ec85e3bdd25b77ea04d936
Tran Van Duy <duytvse183134@fpt.edu.vn>
2025-10-15 14:04:00 +0700

    Update RegisterForm.jsx

--- frontend/src/components/common/inputs/RegisterForm.jsx
+++ frontend/src/components/common/inputs/RegisterForm.jsx
@@ -4,6 +4,8 @@ import { Visibility, VisibilityOff, ArrowForward } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import EmailVerification from '../../auth/EmailVerification';
 import { OtpPurpose } from '../../../utils/emun/Enum';
+import { post } from '../../../utils/api';
+import ApiEndpoints from '../../../utils/api/ApiEndpoints';
 
 export default function RegisterForm({ onShowLogin, initialRole = 1 }) {
     const [showPassword, setShowPassword] = useState(false);
